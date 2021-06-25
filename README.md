5107f44adf4498df095eb67a05f96fc45f048154
Andrea Culqui Carrascal <aculquic@everis.com>
2021-06-24 19:24:17 -0500

    Agregación de  Docker

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 ## Ejercicio 01
 
-### Es un api con Nodejs instalando las librerías express y cowsay al cargar la página carga como titulo Información de Libros y un modo aleatorio, posteriormente en la ruta al agregar /libros carga un json con la descripción de algunos libros  de una petición get.
+### Es un api con Nodejs instalando las librerías express y cowsay al cargar la página imprime "Información de Libros" y un cowsay modo aleatorio, también tiene una petición Get y al rutea con /libros carga un json con la descripción de algunos libros.
 
-
+### Se puede utilizar la solución Dockerizada ejecutando el siguiente comando: 
+### docker run -p 49160:8080 -d aculquic/node-web-app
 
--- index.js
+++ index.js
@@ -1,45 +1,46 @@
 const express = require("express")
 const app = express()
 var cowsay = require("cowsay")
+const PORT = 8081
 
-app.get('/', function (req, res) {
-  res.end(
-    cowsay.say({
-      text : "Información de Libros",
-      e: 'rYa',
-      T: 'L|',
-      r: true,
+app.get('/', function(req, res) {
+    res.end(
+        cowsay.say({
+            text: "Información de Libros",
+            e: 'rYa',
+            T: 'L|',
+            r: true,
 
-  }),
-  )
+        }),
+    )
 });
 
+// Get route
 app.get('/libros', (req, res) => {
-  res.json([
-    { 
-      text: "CAZADORES DE SOMBRAS 1", 
-      author: "CLARE,CASSANDRA",
-      categoria: "Ficción",
-      publicacion: "01-11-2016" 
-    },
-    { 
-      text: "ORGULLO Y PREJUICIO", 
-      author: "AUSTEN,JANE",
-      categoria: "Novela",
-      publicacion: "15-08-2014" 
-    },
-    { 
-      text: "MARFIL", 
-      author: "MERCEDES,RON",
-      categoria: "Ficción adulto joven",
-      publicacion: "23-05-2019" 
-    }
-  ])
+    res.json([{
+            text: "CAZADORES DE SOMBRAS 1",
+            author: "CLARE,CASSANDRA",
+            categoria: "Ficción",
+            publicacion: "01-11-2016"
+        },
+        {
+            text: "ORGULLO Y PREJUICIO",
+            author: "AUSTEN,JANE",
+            categoria: "Novela",
+            publicacion: "15-08-2014"
+        },
+        {
+            text: "MARFIL",
+            author: "MERCEDES,RON",
+            categoria: "Ficción adulto joven",
+            publicacion: "23-05-2019"
+        }
+    ])
 })
 
 
 
 
-app.listen(3000, () => {
- console.log("El servidor está inicializado en el puerto 3000");
+app.listen(PORT, () => {
+    console.log(`El servidor está inicializado en el puerto ${PORT}`)
 });
